8b63cf7dab4c543fa2a18427a918b62e086b4459
Josh Montoya <josh@montoya.io>
2021-01-26 12:48:33 -0800

    update processor pair

--- processor.go
+++ processor.go
@@ -9,12 +9,10 @@ import (
 // Processor will process chunks
 type Processor func(m *Meta, r io.ReadSeeker) error
 
-func newProcessorPairFromFile(filename string) (m *Meta, r io.ReadSeeker, err error) {
-	var f *os.File
+func newProcessorPairFromFile(filename string) (m *Meta, f *os.File, err error) {
 	if f, err = os.Open(filename); err != nil {
 		return
 	}
-	defer f.Close()
 
 	metaBS := make([]byte, metaSize)
 	if _, err = io.ReadAtLeast(f, metaBS, int(metaSize)); err != nil {
@@ -23,10 +21,5 @@ func newProcessorPairFromFile(filename string) (m *Meta, r io.ReadSeeker, err er
 	}
 
 	m = newMetaFromBytes(metaBS)
-
-	if r, err = newReader(f); err != nil {
-		return
-	}
-
 	return
 }
