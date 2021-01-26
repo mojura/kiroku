6c8b6161009a707f8dc992f33dd5cdc11239798a
Josh Montoya <josh@montoya.io>
2021-01-26 09:10:48 -0800

    restructure all the things

--- history.go
+++ history.go
@@ -1,9 +1,12 @@
 package history
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
@@ -13,7 +16,7 @@ import (
 
 // New will initialize a new History instance
 // Note: PostProcessor is optional
-func New(dir, name string, pp PostProcessor) (hp *History, err error) {
+func New(dir, name string, pp Processor) (hp *History, err error) {
 	var h History
 	h.out = scribe.New(fmt.Sprintf("Mojura history (%v)", name))
 	h.dir = dir
@@ -24,9 +27,13 @@ func New(dir, name string, pp PostProcessor) (hp *History, err error) {
 		return
 	}
 
-	h.mc = make(chan *os.File, 12)
-	h.pc = make(chan *processorPayload, 12)
-	h.pp = pp
+	if h.p = pp; h.p == nil {
+		h.p = h.mergeChunk
+	}
+
+	// TODO: Decide if we want to offer the ability to pass a context here.
+	// It might be nice to ensure history instances are properly shut down
+	h.ctx, h.cancelFn = context.WithCancel(context.Background())
 	// Associate returning pointer to created History
 	hp = &h
 	return
@@ -40,90 +47,24 @@ type History struct {
 	// Current chunk
 	c *Chunk
 
+	// Last meta
+	m Meta
+
 	// Directory to store chunks
 	dir string
 	// Name of service
 	name string
 
-	// Merge channel
-	mc chan *os.File
-	// Process channel
-	pc chan *processorPayload
+	// Chunk semaphore
+	cs semaphore
+
 	// Post processing func
-	pp PostProcessor
-}
+	p Processor
 
-func (h *History) deleteChunk(c *Chunk) (err error) {
-	var errs errors.ErrorList
-	errs.Push(c.close())
-	errs.Push(os.Remove(c.filename))
-	return errs.Err()
-}
-
-func (h *History) mergeChunk(f *os.File) (pp *processorPayload, err error) {
-	if _, err = f.Seek(0, 0); err != nil {
-		err = fmt.Errorf("error encountered while seeking to beginning of file: %v", err)
-		return
-	}
-
-	metaBS := make([]byte, metaSize)
-	if _, err = io.ReadAtLeast(f, metaBS, int(metaSize)); err != nil {
-		err = fmt.Errorf("error reading meta bytes: %v", err)
-		return
-	}
-
-	var p processorPayload
-	// Create meta from bytes
-	p.m = newMetafromBytes(metaBS)
-	// Associate chunk file as payload read seeker
-	p.r = f
-
-	if err = h.c.merge(p.m, f); err != nil {
-		err = fmt.Errorf("error encountered while merging: %v", err)
-		return
-	}
-
-	pp = &p
-	return
-}
-
-func (h *History) watchMerge() {
-	for f := range h.mc {
-		var (
-			p   *processorPayload
-			err error
-		)
-
-		if p, err = h.mergeChunk(f); err != nil {
-			h.out.Errorf("error processing chunk: %v", err)
-			// TODO: Figure out best course of action here
-			continue
-		}
-
-		// Send processor payload down processor channel
-		h.pc <- p
-	}
-}
-
-func (h *History) processWatch() {
-	for p := range h.pc {
-		var err error
-		if _, err = p.r.Seek(metaSize, 0); err != nil {
-			// TODO: Figure out best plan of action here
-			h.out.Errorf("error seeking process file: %v", err)
-			continue
-		}
-
-		if err = h.pp(p.m, p.r); err != nil {
-			h.out.Errorf("error running process file: %v", err)
-			continue
-		}
-
-		if err = os.Remove(p.r.Name()); err != nil {
-			h.out.Errorf("error removing processed chunk: %v", err)
-			continue
-		}
-	}
+	// Context of history
+	ctx context.Context
+	// Context cancel func
+	cancelFn func()
 }
 
 // Transaction will engage a new history transaction
@@ -132,12 +73,15 @@ func (h *History) Transaction(fn func(*Chunk) error) (err error) {
 	defer h.mux.Unlock()
 
 	now := time.Now()
-	name := fmt.Sprintf("%s.chunk.%d", h.name, now.UnixNano())
+	unix := now.UnixNano()
+	name := fmt.Sprintf("%s.chunk.%d", h.name, unix)
 
 	var c *Chunk
 	if c, err = newChunk(h.dir, name); err != nil {
 		return
 	}
+
+	c.init(&h.m, unix)
 
 	if err = fn(c); err != nil {
 		if deleteErr := h.deleteChunk(c); deleteErr != nil {
@@ -147,14 +91,147 @@ func (h *History) Transaction(fn func(*Chunk) error) (err error) {
 		return
 	}
 
-	// Add chunk file to merge channel
-	h.mc <- c.f
+	newMeta := *c.m
+	if err = c.close(); err != nil {
+		err = fmt.Errorf("error closing chunk: %v", err)
+		return
+	}
+
+	h.cs.send()
+	h.m = newMeta
 	return
 }
 
 // Close will close the selected instance of History
 func (h *History) Close() (err error) {
+	h.mux.Lock()
+	defer h.mux.Unlock()
+	if h.isClosed() {
+		return errors.ErrIsClosed
+	}
+
 	var errs errors.ErrorList
 	errs.Push(h.c.close())
 	return errs.Err()
+}
+
+func (h *History) getTruncatedName(filename string) (name string) {
+	return strings.Replace(filename, h.dir, "", 1)
+}
+
+func (h *History) getNext() (filename string, ok bool, err error) {
+	fn := walkFn(func(iteratingName string, info os.FileInfo) (err error) {
+		if !h.isChunkMatch(iteratingName, info) {
+			return
+		}
+
+		// We found a match, set <filename> to the iterating name and set <ok> to true
+		filename = iteratingName
+		ok = true
+		return
+	})
+
+	err = filepath.Walk(h.dir, fn)
+	fmt.Printf("Next? <%s> / %v", filename, ok)
+	return
+}
+
+func (h *History) isChunkMatch(filename string, info os.FileInfo) (ok bool) {
+	if info.IsDir() {
+		// We are not interested in directories, return
+		return
+	}
+
+	// Get truncated name
+	name := h.getTruncatedName(filename)
+	if !strings.HasPrefix(name, h.name+".chunk") {
+		// We do not have a service match, return
+		return
+	}
+
+	return true
+}
+
+func (h *History) watch() {
+	var (
+		filename string
+
+		ok  bool
+		err error
+	)
+
+	for !h.isClosed() {
+		if filename, ok, err = h.getNext(); err != nil {
+			// TODO: Get teams input on if this value should be configurable
+			h.out.Errorf("error getting next chunk filename: <%v>, sleeping for a minute and trying again", err)
+			time.Sleep(time.Minute)
+			continue
+		}
+
+		if !ok {
+			h.waitForNext()
+			continue
+		}
+
+		if err = h.processChunk(filename); err != nil {
+			// TODO: Get teams input on the best course of action here
+			h.out.Errorf("error encountered during processing chunk: <%v>, sleeping for a minute and trying again", err)
+			time.Sleep(time.Minute)
+		}
+	}
+}
+
+func (h *History) waitForNext() {
+	select {
+	case <-h.cs:
+	case <-h.ctx.Done():
+	}
+}
+
+func (h *History) processChunk(filename string) (err error) {
+	var (
+		m *Meta
+		r io.ReadSeeker
+	)
+
+	if m, r, err = newProcessorPairFromFile(filename); err != nil {
+		return
+	}
+
+	if err = h.p(m, r); err != nil {
+		err = fmt.Errorf("error encountered during processing action: <%v>", err)
+		return
+	}
+
+	if err = os.Remove(filename); err != nil {
+		err = fmt.Errorf("error encountered while removing file: <%v>", err)
+		return
+	}
+
+	return
+}
+
+func (h *History) mergeChunk(m *Meta, r io.ReadSeeker) (err error) {
+	if err = h.c.merge(m, r); err != nil {
+		err = fmt.Errorf("error encountered while merging: %v", err)
+		return
+	}
+
+	return
+}
+
+func (h *History) deleteChunk(c *Chunk) (err error) {
+	var errs errors.ErrorList
+	errs.Push(c.close())
+	errs.Push(os.Remove(c.filename))
+	return errs.Err()
+}
+
+func (h *History) isClosed() bool {
+	select {
+	case <-h.ctx.Done():
+		return true
+	default:
+		return false
+	}
 }
