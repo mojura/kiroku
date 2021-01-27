91d7a1a2eb97105d44af95a45f3d6388279e926f
Josh Montoya <josh@montoya.io>
2021-01-26 19:49:39 -0800

    add test case for Meta method

--- reader_test.go
+++ reader_test.go
@@ -6,41 +6,83 @@ import (
 	"testing"
 )
 
+var readerTestcases = []readerTestcase{
+	{
+		t:     TypeWriteAction,
+		data:  "foo 1",
+		index: 1,
+	},
+	{
+		t:     TypeWriteAction,
+		data:  "foo 2",
+		index: 2,
+	},
+	{
+		t:     TypeWriteAction,
+		data:  "foo 3",
+		index: 3,
+	},
+	{
+		t:     TypeDeleteAction,
+		data:  "",
+		index: 3,
+	},
+}
+
+func TestReader_Meta(t *testing.T) {
+	var (
+		c   *Writer
+		err error
+	)
+
+	tcs := readerTestcases
+	if err = os.Mkdir("./test_data", 0744); err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer os.RemoveAll("./test_data")
+
+	if c, err = newWriter("./test_data", "testie"); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	for _, tc := range tcs {
+		if err = c.AddRow(tc.t, []byte(tc.data)); err != nil {
+			t.Fatal(err)
+		}
+
+		c.SetIndex(tc.index)
+	}
+
+	if _, err = c.f.Seek(0, 0); err != nil {
+		t.Fatalf("error setting file to start: %v", err)
+		return
+	}
+
+	var r *Reader
+	if r, err = NewReader(c.f); err != nil {
+		t.Fatalf("error initializing reader: %v", err)
+		return
+	}
+
+	meta := r.Meta()
+	if last := tcs[len(tcs)-1]; meta.CurrentIndex != last.index {
+		t.Fatalf("invalid index, expected %d and received %d", last.index, meta.CurrentIndex)
+	}
+
+	if meta.RowCount != int64(len(tcs)) {
+		t.Fatalf("invalid row count, expected %d and received %d", len(tcs), meta.RowCount)
+	}
+}
+
 func TestReader_ForEach(t *testing.T) {
 	var (
 		c   *Writer
 		err error
 	)
 
-	type testcase struct {
-		t     Type
-		data  string
-		index int64
-	}
-
-	tcs := []testcase{
-		{
-			t:     TypeWriteAction,
-			data:  "foo 1",
-			index: 1,
-		},
-		{
-			t:     TypeWriteAction,
-			data:  "foo 2",
-			index: 2,
-		},
-		{
-			t:     TypeWriteAction,
-			data:  "foo 3",
-			index: 3,
-		},
-		{
-			t:     TypeDeleteAction,
-			data:  "",
-			index: 3,
-		},
-	}
-
+	tcs := readerTestcases
 	if err = os.Mkdir("./test_data", 0744); err != nil {
 		t.Fatal(err)
 		return
@@ -89,13 +131,10 @@ func TestReader_ForEach(t *testing.T) {
 	if count != len(tcs) {
 		t.Fatalf("invalid number of iterations, expected %d and received %d", len(tcs), count)
 	}
+}
 
-	meta := r.Meta()
-	if last := tcs[len(tcs)-1]; meta.CurrentIndex != last.index {
-		t.Fatalf("invalid index, expected %d and received %d", last.index, meta.CurrentIndex)
-	}
-
-	if meta.RowCount != int64(len(tcs)) {
-		t.Fatalf("invalid row count, expected %d and received %d", len(tcs), meta.RowCount)
-	}
+type readerTestcase struct {
+	t     Type
+	data  string
+	index int64
 }
