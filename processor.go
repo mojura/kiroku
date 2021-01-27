af3e1427ecf4eae932c214db208bd387e3694960
Josh Montoya <josh@montoya.io>
2021-01-27 15:43:35 -0800

    check to ensure chunk is of minimum size

--- processor.go
+++ processor.go
@@ -14,6 +14,17 @@ func newProcessorPair(filename string) (m *Meta, f *os.File, err error) {
 		return
 	}
 
+	var info os.FileInfo
+	if info, err = f.Stat(); err != nil {
+		err = fmt.Errorf("error getting file info for <%s>: %v", filename, err)
+		return
+	}
+
+	if info.Size() < metaSize {
+		err = fmt.Errorf("file size for <%s> of %d bytes is smaller than the minimum of %d", filename, info.Size(), metaSize)
+		return
+	}
+
 	metaBS := make([]byte, metaSize)
 	if _, err = io.ReadAtLeast(f, metaBS, int(metaSize)); err != nil {
 		err = fmt.Errorf("error reading meta bytes: %v", err)
