a093827905ff5585e8bbbb6a7d7739c578720f42
Josh Montoya <josh@montoya.io>
2021-01-27 16:05:55 -0800

    expand tests

--- kiroku_test.go
+++ kiroku_test.go
@@ -2,13 +2,64 @@ package history
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"os"
 	"testing"
-	"time"
 )
 
-func TestKiroku_Transaction(t *testing.T) {
+func TestKiroku_Transaction_with_standard_processor(t *testing.T) {
+	var (
+		k   *Kiroku
+		err error
+	)
+
+	if err = os.Mkdir("test_data", 0744); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll("./test_data")
+
+	if k, err = New("test_data", "tester", nil); err != nil {
+		t.Fatal(err)
+	}
+	defer k.Close()
+
+	if err = k.Transaction(func(w *Writer) (err error) {
+		w.SetIndex(1337)
+		w.AddRow(TypeWriteAction, []byte("hello world!"))
+		return
+	}); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if err = k.Close(); err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer k.Close()
+
+	if k, err = New("test_data", "tester", nil); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if err = k.Transaction(func(w *Writer) (err error) {
+		if index := w.GetIndex(); index != 1337 {
+			err = fmt.Errorf("invalid index, expected %d and received %d", 1337, index)
+			return
+		}
+
+		return
+	}); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	k.Close()
+}
+
+func TestKiroku_Transaction_with_custom_processor(t *testing.T) {
 	var (
 		k   *Kiroku
 		err error
@@ -41,5 +92,26 @@ func TestKiroku_Transaction(t *testing.T) {
 		return
 	}
 
-	time.Sleep(time.Second * 5)
+	if err = k.Close(); err != nil {
+		t.Fatal(err)
+	}
+
+	if k, err = New("test_data", "tester", nil); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if err = k.Transaction(func(w *Writer) (err error) {
+		if index := w.GetIndex(); index != 1337 {
+			err = fmt.Errorf("invalid index, expected %d and received %d", 1337, index)
+			return
+		}
+
+		return
+	}); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	k.Close()
 }
