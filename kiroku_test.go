daef5c91eea430f6daec4a1733f470b14520f2da
Josh Montoya <josh@montoya.io>
2021-02-26 09:49:22 -0800

    implement snapshot test

--- kiroku_test.go
+++ kiroku_test.go
@@ -190,6 +190,67 @@ func TestKiroku_Transaction_with_custom_processor(t *testing.T) {
 	k.Close()
 }
 
+func TestKiroku_Snapshot(t *testing.T) {
+	var (
+		k   *Kiroku
+		err error
+	)
+
+	if err = os.Mkdir("test_data", 0744); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll("./test_data")
+
+	if k, err = New("test_data", "tester", nil, nil); err != nil {
+		t.Fatal(err)
+	}
+	defer k.Close()
+
+	if err = k.Transaction(func(t *Transaction) (err error) {
+		if err = t.SetIndex(1337); err != nil {
+			return
+		}
+
+		if err = t.AddBlock(TypeWriteAction, []byte("0"), []byte("hello world!")); err != nil {
+			return
+		}
+
+		if err = t.AddBlock(TypeDeleteAction, []byte("0"), []byte("hello world!")); err != nil {
+			return
+		}
+
+		if err = t.AddBlock(TypeWriteAction, []byte("1"), []byte("hello world!")); err != nil {
+			return
+		}
+
+		return
+	}); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if err = k.Snapshot(func(s *Snapshot) (err error) {
+		if err = s.Write([]byte("1"), []byte("hello world!")); err != nil {
+			return
+		}
+
+		return
+	}); err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	var m Meta
+	if m, err = k.Meta(); err != nil {
+		t.Fatal(err)
+	}
+
+	switch {
+	case m.BlockCount != 1:
+		t.Fatalf("invalid block count, expected %d and received %d", 1, m.BlockCount)
+	}
+}
+
 func ExampleNew() {
 	var err error
 	if testKiroku, err = New("./test_data", "tester", nil, nil); err != nil {
