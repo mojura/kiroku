84aa3b24cbd85cf5f1859c1557546ad5bbedb690
Josh Montoya <josh@montoya.io>
2021-09-06 14:14:19 -0700

    add dir and name to options

--- options.go
+++ options.go
@@ -1,8 +1,33 @@
 package kiroku
 
+import "github.com/hatchify/errors"
+
+const (
+	// ErrEmptyDirectory is returned when a directory is empty
+	ErrEmptyDirectory = errors.Error("invalid directory, cannot be empty")
+	// ErrEmptyName is returned when a name is empty
+	ErrEmptyName = errors.Error("invalid name, cannot be empty")
+)
+
 // Options represent Kiroku options
 type Options struct {
+	Dir  string
+	Name string
+
 	AvoidMergeOnInit   bool
 	AvoidMergeOnClose  bool
 	AvoidExportOnClose bool
+}
+
+func (o *Options) Validate() (err error) {
+	var errs errors.ErrorList
+	if len(o.Dir) == 0 {
+		errs.Push(ErrEmptyDirectory)
+	}
+
+	if len(o.Name) == 0 {
+		errs.Push(ErrEmptyName)
+	}
+
+	return errs.Err()
 }
