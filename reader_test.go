c63403d704d3396ff723c707e1a13da50f402d6a
Josh Montoya <josh@montoya.io>
2021-09-09 17:31:46 -0700

    change test to use Read

--- reader_test.go
+++ reader_test.go
@@ -220,23 +220,33 @@ func TestReader_ForEach_with_active_writes(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	var r *Reader
-	if r, err = NewReader(c.f); err != nil {
-		t.Fatalf("error initializing reader: %v", err)
-		return
-	}
-
 	var lastPosition int64
-	if lastPosition, err = testForEach(r, tcs[:2], 0); err != nil {
-		t.Fatal(err)
+	if err = Read(c.Filename(), func(r *Reader) (err error) {
+		if lastPosition, err = testForEach(r, tcs[:2], 0); err != nil {
+			t.Fatal(err)
+		}
+
+		return
+
+	}); err != nil {
+		return
 	}
 
 	if err = populateReaderTestcase(c, tcs[2:]); err != nil {
 		t.Fatal(err)
 	}
 
-	if _, err = testForEach(r, tcs[2:], lastPosition); err != nil {
-		t.Fatal(err)
+	c.mm.Flush()
+
+	if err = Read(c.Filename(), func(r *Reader) (err error) {
+		if _, err = testForEach(r, tcs[2:], lastPosition); err != nil {
+			t.Fatal(err)
+		}
+
+		return
+
+	}); err != nil {
+		return
 	}
 }
 
@@ -656,8 +666,13 @@ func testForEach(r *Reader, tcs []readerTestcase, seek int64) (lastPosition int6
 	var count int
 	if lastPosition, err = r.ForEach(seek, func(b *Block) (err error) {
 		tc := tcs[count]
+		if str := string(b.Key); str != tc.key {
+			err = fmt.Errorf("invalid key, expected <%s> and received <%s>", tc.key, str)
+			return
+		}
+
 		if str := string(b.Value); str != tc.value {
-			err = fmt.Errorf("invalid data, expected <%s> and received <%s>", tc.value, str)
+			err = fmt.Errorf("invalid value, expected <%s> and received <%s>", tc.value, str)
 			return
 		}
 
@@ -673,6 +688,7 @@ func testForEach(r *Reader, tcs []readerTestcase, seek int64) (lastPosition int6
 		return
 	}
 
+	lastPosition = r.Meta().TotalBlockSize
 	return
 }
 
@@ -697,11 +713,6 @@ func populateReaderTestcase(w *Writer, tcs []readerTestcase) (err error) {
 		}
 
 		tcs[i].lastBlockSize = w.m.TotalBlockSize
-	}
-
-	if _, err = w.f.Seek(0, 0); err != nil {
-		err = fmt.Errorf("error setting file to start: %v", err)
-		return
 	}
 
 	return
