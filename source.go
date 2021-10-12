014586328b9a56b7e1775571c8c762ab9f9c0d85
Josh Montoya <josh@montoya.io>
2021-10-12 14:56:40 -0700

    adjust source to utilize Get instead of GetLastSnapshot

--- source.go
+++ source.go
@@ -8,7 +8,7 @@ import (
 // Source is used for importing
 type Source interface {
 	Export(ctx context.Context, filename string, r io.Reader) error
-	Import(ctx context.Context, filename string, w io.Writer) (err error)
+	Import(ctx context.Context, filename string, w io.Writer) error
+	Get(ctx context.Context, filename string, fn func(io.Reader) error) error
 	GetNext(ctx context.Context, prefix, lastFilename string) (filename string, err error)
-	GetLastSnapshot(ctx context.Context, prefix string) (filename string, err error)
 }
--- source_test.go
+++ source_test.go
@@ -5,20 +5,20 @@ import (
 	"io"
 )
 
-func newMockSource(e exportFn, i importFn, gn getNextFn, gl getLastSnapshotFn) *mockSource {
+func newMockSource(e exportFn, i importFn, g getFn, gn getNextFn) *mockSource {
 	var m mockSource
 	m.exportFn = e
 	m.importFn = i
+	m.getFn = g
 	m.getNextFn = gn
-	m.getLastSnapshotFn = gl
 	return &m
 }
 
 type mockSource struct {
-	exportFn          exportFn
-	importFn          importFn
-	getNextFn         getNextFn
-	getLastSnapshotFn getLastSnapshotFn
+	exportFn  exportFn
+	importFn  importFn
+	getFn     getFn
+	getNextFn getNextFn
 }
 
 func (m *mockSource) Export(ctx context.Context, filename string, r io.Reader) error {
@@ -29,18 +29,18 @@ func (m *mockSource) Import(ctx context.Context, filename string, w io.Writer) (
 	return m.importFn(ctx, filename, w)
 }
 
+func (m *mockSource) Get(ctx context.Context, filename string, fn func(io.Reader) error) (err error) {
+	return m.getFn(ctx, filename, fn)
+}
+
 func (m *mockSource) GetNext(ctx context.Context, prefix, lastFilename string) (filename string, err error) {
 	return m.getNextFn(ctx, prefix, lastFilename)
 }
 
-func (m *mockSource) GetLastSnapshot(ctx context.Context, prefix string) (filename string, err error) {
-	return m.getLastSnapshotFn(ctx, prefix)
-}
-
 type exportFn func(ctx context.Context, filename string, r io.Reader) error
-type importFn func(ctx context.Context, filename string, w io.Writer) (err error)
+type importFn func(ctx context.Context, filename string, w io.Writer) error
+type getFn func(ctx context.Context, filename string, fn func(io.Reader) error) error
 type getNextFn func(ctx context.Context, prefix, lastFilename string) (filename string, err error)
-type getLastSnapshotFn func(ctx context.Context, prefix string) (filename string, err error)
 
 func newErrorSource(srcErr error) *mockSource {
 	e := func(ctx context.Context, filename string, r io.Reader) (err error) {
@@ -51,13 +51,13 @@ func newErrorSource(srcErr error) *mockSource {
 		return srcErr
 	}
 
+	g := func(ctx context.Context, filename string, fn func(io.Reader) error) (err error) {
+		return srcErr
+	}
+
 	gn := func(ctx context.Context, prefix, lastFilename string) (filename string, err error) {
 		return "", srcErr
 	}
 
-	gl := func(ctx context.Context, prefix string) (filename string, err error) {
-		return "", srcErr
-	}
-
-	return newMockSource(e, i, gn, gl)
+	return newMockSource(e, i, g, gn)
 }
