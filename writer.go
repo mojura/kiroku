4b8fcfd49a44cbe860be888a4cdf28fe30215b47
Josh Montoya <josh@montoya.io>
2023-08-17 11:26:49 -0700

    updated writer

--- writer.go
+++ writer.go
@@ -1,7 +1,6 @@
 package kiroku
 
 import (
-	"io"
 	"os"
 	"path"
 	"sync"
@@ -12,45 +11,19 @@ import (
 
 var ErrEmptyBlock = errors.New("invalid block, cannot be empty")
 
-// NewWriter will initialize a new Writer instance
-func NewWriter(dir, name string) (wp *Writer, err error) {
-	// Create a new writer
-	if wp, err = newWriter(dir, name); err != nil {
-		return
-	}
-
-	return
-}
-
-func newWriter(dir, name string) (wp *Writer, err error) {
-	var f *os.File
+func newWriter(dir string, filename Filename) (wp *Writer, err error) {
+	var w Writer
+	w.filename = filename
 	// Set filename as a combination of the provided directory, name, and a .kir extension
-	filename := path.Join(dir, name+".kir")
+	w.filepath = path.Join(dir, w.filename.String())
 	// Open target file
 	// Note: This will create the file if it does not exist
-	if f, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0744); err != nil {
-		return
-	}
-
-	return NewWriterWithFile(f)
-}
-
-// NewWriterWithFile will initialize a new Writer instance
-func NewWriterWithFile(f *os.File) (wp *Writer, err error) {
-	var w Writer
-	w.f = f
-	w.filename = f.Name()
-	// Whenever function ends, close the Writer if an error was encountered
-	defer func() { w.closeIfError(err) }()
-
-	// Move to the end of the file
-	if _, err = w.f.Seek(0, io.SeekEnd); err != nil {
+	if w.f, err = os.OpenFile(w.filepath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0744); err != nil {
 		return
 	}
 
 	// Initialize enkodo writer
 	w.w = enkodo.NewWriter(w.f)
-	// Associate returning pointer to created Writer
 	wp = &w
 	return
 }
@@ -65,7 +38,8 @@ type Writer struct {
 	w *enkodo.Writer
 
 	// Location of file
-	filename string
+	filename Filename
+	filepath string
 
 	blockCount int
 
@@ -94,11 +68,6 @@ func (w *Writer) Write(value Block) (err error) {
 	return
 }
 
-// Filename will return the underlying filename of the Writer
-func (w *Writer) Filename() string {
-	return w.filename
-}
-
 // Close will close a writer
 func (w *Writer) Close() (err error) {
 	w.mux.Lock()
@@ -120,13 +89,4 @@ func (w *Writer) Close() (err error) {
 	}
 
 	return errs.Err()
-}
-
-// Close will close the selected instance of Writer
-func (w *Writer) closeIfError(err error) {
-	if err == nil {
-		return
-	}
-
-	w.Close()
 }
