089a036dddf16211919471d1314de360878a74c1
Josh Montoya <josh@montoya.io>
2023-08-18 10:58:32 -0700

    implement noop tests

---
+++ noop_test.go
@@ -0,0 +1,137 @@
+package kiroku
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestNOOP_Export(t *testing.T) {
+	type args struct {
+		filename string
+		r        io.Reader
+	}
+
+	type testcase struct {
+		name    string
+		args    args
+		wantErr bool
+	}
+
+	tests := []testcase{
+		{
+			args: args{
+				filename: "test",
+				r:        strings.NewReader("hello world"),
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NOOP{}
+			if err := n.Export(context.Background(), tt.args.filename, tt.args.r); (err != nil) != tt.wantErr {
+				t.Errorf("NOOP.Export() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNOOP_Import(t *testing.T) {
+	type args struct {
+		filename string
+		w        io.Writer
+	}
+
+	type testcase struct {
+		name    string
+		args    args
+		wantErr bool
+	}
+
+	tests := []testcase{
+		{
+			args: args{
+				filename: "test",
+				w:        bytes.NewBuffer(nil),
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NOOP{}
+			if err := n.Import(context.Background(), tt.args.filename, tt.args.w); (err != nil) != tt.wantErr {
+				t.Errorf("NOOP.Export() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNOOP_Get(t *testing.T) {
+	type args struct {
+		filename string
+	}
+
+	type testcase struct {
+		name    string
+		args    args
+		wantErr bool
+	}
+
+	tests := []testcase{
+		{
+			args: args{
+				filename: "test",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NOOP{}
+			if err := n.Get(context.Background(), tt.args.filename, func(r io.Reader) error {
+				return nil
+			}); (err != nil) != tt.wantErr {
+				t.Errorf("NOOP.Export() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestNOOP_GetNext(t *testing.T) {
+	type args struct {
+		prefix       string
+		lastFilename string
+	}
+
+	type testcase struct {
+		name    string
+		args    args
+		wantErr bool
+	}
+
+	tests := []testcase{
+		{
+			args: args{
+				prefix:       "test",
+				lastFilename: "test_000",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NOOP{}
+			if _, err := n.GetNext(context.Background(), tt.args.prefix, tt.args.lastFilename); (err != nil) != tt.wantErr {
+				t.Errorf("NOOP.Export() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
