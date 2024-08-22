db162c2a03b5afd535c2231762f6daf8075b7afd
Josh Montoya <josh@montoya.io>
2024-08-22 07:46:52 -0700

    adjust mock source

--- source_test.go
+++ source_test.go
@@ -15,10 +15,11 @@ func newMockSource(e exportFn, i importFn, g getFn, gn getNextFn) *mockSource {
 }
 
 type mockSource struct {
-	exportFn  exportFn
-	importFn  importFn
-	getFn     getFn
-	getNextFn getNextFn
+	exportFn      exportFn
+	importFn      importFn
+	getFn         getFn
+	getNextFn     getNextFn
+	getNextListFn getNextListFn
 }
 
 func (m *mockSource) Export(ctx context.Context, prefix, filename string, r io.Reader) (string, error) {
@@ -37,7 +38,12 @@ func (m *mockSource) GetNext(ctx context.Context, prefix, lastFilename string) (
 	return m.getNextFn(ctx, prefix, lastFilename)
 }
 
+func (m *mockSource) GetNextList(ctx context.Context, prefix, lastFilename string, maxKeys int64) (filenames []string, err error) {
+	return m.getNextListFn(ctx, prefix, lastFilename, maxKeys)
+}
+
 type exportFn func(ctx context.Context, prefix, filename string, r io.Reader) (string, error)
 type importFn func(ctx context.Context, prefix, filename string, w io.Writer) error
 type getFn func(ctx context.Context, prefix, filename string, fn func(io.Reader) error) error
 type getNextFn func(ctx context.Context, prefix, lastFilename string) (filename string, err error)
+type getNextListFn func(ctx context.Context, prefix, lastFilename string, maxKeys int64) (filenames []string, err error)
